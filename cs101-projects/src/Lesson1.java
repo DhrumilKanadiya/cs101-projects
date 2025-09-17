0b522c946e380efe144486667e8de36efbc20fd3
Dhrumil Kanadiya <dhrumilk3080@gmail.com>
2025-09-17 15:20:52 -0700

    Lesson 1 Part 1 = Programming Skeleton/Variables

--- cs101-projects/src/Lesson1.java
+++ cs101-projects/src/Lesson1.java
@@ -1,5 +1,17 @@
 public class Lesson1 {
     public static void main(String[] args) {
+        // Step 1: print hello
         System.out.println("Hello, Dhrumil 🚀");
+
+        // Step 2: variables
+        int age = 20;
+        double gpa = 3.8;
+        String name = "Dhrumil";
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Student: " + isStudent);
     }
 }
