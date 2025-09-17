512f27c3605e29305a67faa803472bcd0f6f0223
Dhrumil Kanadiya <dhrumilk3080@gmail.com>
2025-09-17 14:52:04 -0700

    Lesson 1 Part 1 = Programming Skeleton

---
+++ cs101-projects/src/Lesson1part1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Lesson1part1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // step:1 Hello world
+        System.out.println("Hello World!");
+
+        // step:2 variables
+
+        int age = 20;
+        double gpa = 3.8;
+        String name = "Dhrumil";
+        boolean Isstudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Name: " + name);
+        System.out.println("IsStudent: " + Isstudent);
+    }
+}
